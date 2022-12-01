36012e8f999669823f65156387ca32bbcd3ffefb
Kyle Hench <kylehench@gmail.com>
2022-12-01 14:41:24 -0800

    added tailwind as npm package, VS Code task

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: ["./flask_app/templates/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
