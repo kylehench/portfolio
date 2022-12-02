1607145c4e09c82cf978cf80e342f33860bbe2bb
Kyle Hench <kylehench@gmail.com>
2022-12-01 18:02:45 -0800

    initial navbar, about section

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Development - set up environment
 `pipenv install flask`
+
+# Attributions
+Photo by <a href="https://unsplash.com/@szamanm?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Piotr Musioł</a> on <a href="https://unsplash.com/s/photos/silicon-valley?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
   content: ["./flask_app/templates/**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg:'976px',
+      xl:'1440px'
+    },
+    extend: {
+      colors: {
+        lightBlue: '#CCD9E2',
+        blue: '#285185',
+        orange: '#D67940',
+        brown: '#6F4849',
+      },
+    },
   },
   plugins: [],
 }
